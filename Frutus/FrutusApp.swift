b6691d14595a2fbc1b2a2562b32d4f71514161c1
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-11-28 23:21:08 +0200

    Initial Commit

---
+++ Frutus/FrutusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutusApp.swift
+//  Frutus
+//
+//  Created by Quest76 on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FrutusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
